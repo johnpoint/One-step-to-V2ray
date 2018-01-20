95123aff5ea5275a59e66e4b679dcd381343523f
johnpoint <jahanngauss414@gmail.com>
2018-01-20 19:06:23 +0800

    # fix

--- v2ray.sh
+++ v2ray.sh
@@ -4,14 +4,14 @@ export PATH
 
 #=================================================
 #	System Required: Ubuntu 14.04+
-#	Version: 2.2.5
+#	Version: 2.2.6
 #	Blog: johnpoint.github.io
 #	Author: johnpoint
 #    USE AT YOUR OWN RISK!!!
 #    Publish under GNU General Public License v2
 #=================================================
 
-sh_ver="2.2.5"
+sh_ver="2.2.6"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
@@ -537,10 +537,6 @@ fi
 		\"port\": ${port},
 		\"protocol\":\"socks\",
 		\"settings\":{${authconf}
- 		 \"udp\": false,
- 		 \"ip\": \"127.0.0.1\",
- 		 \"timeout\": 0,
- 		 \"userLevel\": 0
 	  }
   },
     \"outbound\": {
