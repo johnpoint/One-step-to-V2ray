d6a144d0cc7637601ce63d4124bfac72902dd3da
johnpoint <jahanngauss414@gmail.com>
2018-01-20 19:15:56 +0800

    # fix

--- v2ray.sh
+++ v2ray.sh
@@ -4,14 +4,14 @@ export PATH
 
 #=================================================
 #	System Required: Ubuntu 14.04+
-#	Version: 2.2.6
+#	Version: 2.2.7
 #	Blog: johnpoint.github.io
 #	Author: johnpoint
 #    USE AT YOUR OWN RISK!!!
 #    Publish under GNU General Public License v2
 #=================================================
 
-sh_ver="2.2.6"
+sh_ver="2.2.7"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
@@ -361,7 +361,7 @@ elif [[ ${cauth} == '2' ]]; then
 						\"user\":\"${username}\",
 						\"pass\":\"${pw}\"
 					},
-				],
+				]
 "
 else
 	echo -e "${Error} 输入错误，请重试~"
