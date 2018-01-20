cac0470e875a524a1dc78dc2282adb8150b14344
johnpoint <jahanngauss414@gmail.com>
2018-01-20 18:56:42 +0800

    # fix

--- v2ray.sh
+++ v2ray.sh
@@ -4,14 +4,14 @@ export PATH
 
 #=================================================
 #	System Required: Ubuntu 14.04+
-#	Version: 2.2.4
+#	Version: 2.2.5
 #	Blog: johnpoint.github.io
 #	Author: johnpoint
 #    USE AT YOUR OWN RISK!!!
 #    Publish under GNU General Public License v2
 #=================================================
 
-sh_ver="2.2.4"
+sh_ver="2.2.5"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
@@ -342,7 +342,7 @@ ifmux='false'
 read cauth
 if [[ ${cauth} == '1' ]]; then
 	auth='none'
-	authconf='		"auth":"${auth}",'
+	authconf='		\"auth\":\"${auth}\",'
 elif [[ ${cauth} == '2' ]]; then
 	auth='password'
 	echo "输入用户名"
@@ -354,15 +354,15 @@ elif [[ ${cauth} == '2' ]]; then
 	用户名：${username}
 	密码：${pw}
 	——————————————————————"
-	authconf='
-	   			"auth":"${auth}",
-   			"accounts":[
+	authconf="
+	   			\"auth\":\"${auth}\",
+   			\"accounts\":[
 					{
-						"user":"${username}",
-						"pass":"${pw}"
+						\"user\":\"${username}\",
+						\"pass\":\"${pw}\"
 					},
 				],
-'
+"
 else
 	echo -e "${Error} 输入错误，请重试~"
 	Set_auth
