67ceb8bf82d806256c823f3aee9cde5755f32f00
johnpoint <jahanngauss414@gmail.com>
2018-01-21 09:44:36 +0800

    # update

--- v2ray.sh
+++ v2ray.sh
@@ -4,14 +4,14 @@ export PATH
 
 #=================================================
 #	System Required: Ubuntu 14.04+
-#	Version: 2.2.9
+#	Version: 2.2.10
 #	Blog: johnpoint.github.io
 #	Author: johnpoint
 #    USE AT YOUR OWN RISK!!!
 #    Publish under GNU General Public License v2
 #=================================================
 
-sh_ver="2.2.9"
+sh_ver="2.2.10"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
@@ -345,7 +345,18 @@ ifmux='false'
 read cauth
 if [[ ${cauth} == '1' ]]; then
 	auth='none'
-	authconf='		\"auth\":\"${auth}\",'
+	authconf="  			\"auth\": \"none\",
+  			\"accounts\": [
+    			{
+      				\"user\": \"\",
+      				\"pass\": \"\"
+    			}
+  			],
+  		\"udp\": false,
+ 		 \"ip\": \"127.0.0.1\",
+  		\"timeout\": 0,
+ 		 \"userLevel\": 0
+		}"
 elif [[ ${cauth} == '2' ]]; then
 	auth='password'
 	echo "输入用户名"
@@ -358,14 +369,18 @@ elif [[ ${cauth} == '2' ]]; then
 	密码：${pw}
 	——————————————————————"
 	authconf="
-	   			\"auth\":\"${auth}\",
-					\"port\": ${port},
-   			\"accounts\":[
-					{
-						\"user\":\"${username}\",
-						\"pass\":\"${pw}\"
-					}
-				],
+	  			\"auth\": \"password\",
+  			\"accounts\": [
+    			{
+      				\"user\": \"${username}\",
+      				\"pass\": \"${pw}\"
+    			}
+  			],
+  		\"udp\": false,
+ 		 \"ip\": \"127.0.0.1\",
+  		\"timeout\": 0,
+ 		 \"userLevel\": 0
+		}
 "
 else
 	echo -e "${Error} 输入错误，请重试~"
@@ -530,27 +545,24 @@ fi
  Save_socks(){
  Stop
   echo -e "${Info}正在保存配置~"
- echo "
-{
+ echo "{
 	\"log\":{
     	\"loglevel\": \"${loglv}\",
     	\"access\": \"/var/log/v2ray/access.log\",
     	\"error\": \"/var/log/v2ray/error.log\"
   	},
   	\"inbound\": {
+		\"port\": ${port},
 		\"protocol\":\"socks\",
-		\"settings\":{${authconf}
-  \"udp\": false,
-  \"ip\": \"127.0.0.1\",
-  \"timeout\": 0,
-  \"userLevel\": 0
-	  }
+		\"settings\":{
+${authconf}
   },
     \"outbound\": {
     \"protocol\": \"freedom\",
     \"settings\": {}
   }
-}" > /etc/v2ray/config.json
+}
+" > /etc/v2ray/config.json
 echo -e "${Info} 配置完成"
 Start
  }
