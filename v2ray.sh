ddd2246e5c0fdb06d93d8ddc7e139211b01da7da
johnpoint <jahanngauss414@gmail.com>
2018-01-21 09:56:54 +0800

    # update

--- v2ray.sh
+++ v2ray.sh
@@ -4,14 +4,14 @@ export PATH
 
 #=================================================
 #	System Required: Ubuntu 14.04+
-#	Version: 2.2.10
+#	Version: 2.2.11
 #	Blog: johnpoint.github.io
 #	Author: johnpoint
 #    USE AT YOUR OWN RISK!!!
 #    Publish under GNU General Public License v2
 #=================================================
 
-sh_ver="2.2.10"
+sh_ver="2.2.11"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
@@ -368,8 +368,7 @@ elif [[ ${cauth} == '2' ]]; then
 	用户名：${username}
 	密码：${pw}
 	——————————————————————"
-	authconf="
-	  			\"auth\": \"password\",
+	authconf="		\"auth\": \"password\",
   			\"accounts\": [
     			{
       				\"user\": \"${username}\",
