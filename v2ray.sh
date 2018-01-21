ff9cd118c798948ecb883d6f4ccaa31446af0a8e
johnpoint <jahanngauss414@gmail.com>
2018-01-21 09:23:55 +0800

    # fix

--- v2ray.sh
+++ v2ray.sh
@@ -4,14 +4,14 @@ export PATH
 
 #=================================================
 #	System Required: Ubuntu 14.04+
-#	Version: 2.2.8
+#	Version: 2.2.9
 #	Blog: johnpoint.github.io
 #	Author: johnpoint
 #    USE AT YOUR OWN RISK!!!
 #    Publish under GNU General Public License v2
 #=================================================
 
-sh_ver="2.2.8"
+sh_ver="2.2.9"
 Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
 Info="${Green_font_prefix}[信息]${Font_color_suffix}"
 Error="${Red_font_prefix}[错误]${Font_color_suffix}"
@@ -359,12 +359,13 @@ elif [[ ${cauth} == '2' ]]; then
 	——————————————————————"
 	authconf="
 	   			\"auth\":\"${auth}\",
+					\"port\": ${port},
    			\"accounts\":[
 					{
 						\"user\":\"${username}\",
 						\"pass\":\"${pw}\"
 					}
-				]
+				],
 "
 else
 	echo -e "${Error} 输入错误，请重试~"
@@ -537,9 +538,12 @@ fi
     	\"error\": \"/var/log/v2ray/error.log\"
   	},
   	\"inbound\": {
-		\"port\": ${port},
 		\"protocol\":\"socks\",
 		\"settings\":{${authconf}
+  \"udp\": false,
+  \"ip\": \"127.0.0.1\",
+  \"timeout\": 0,
+  \"userLevel\": 0
 	  }
   },
     \"outbound\": {
